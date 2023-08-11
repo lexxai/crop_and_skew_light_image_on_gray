8b39b735e31e75936b1888cedbd7ea6e665920cf
lexxai <lexxai@users.noreply.github.com>
2023-08-11 09:48:47 +0300

    buid

--- script/build.cmd
+++ script/build.cmd
@@ -1,7 +1,9 @@
+python build-version.py
+
 mkdir "../pyinstall"
 ERASE "../pyinstall" /S/Q
 PUSHD "../pyinstall"
 
-python build-version.py
+x
 pyinstaller "../ai_crop_images/main.py" --clean --name ai_crop_images --onefile --version-file "../versionfile.txt" 
 POPD
