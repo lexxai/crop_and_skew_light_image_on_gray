22d9e2fdb02072f670b31f9993c37964fe07ce22
lexxai <lexxai@users.noreply.github.com>
2023-08-11 09:47:08 +0300

    tune build

--- script/build.cmd
+++ script/build.cmd
@@ -2,5 +2,6 @@ mkdir "../pyinstall"
 ERASE "../pyinstall" /S/Q
 PUSHD "../pyinstall"
 
+python build-version.py
 pyinstaller "../ai_crop_images/main.py" --clean --name ai_crop_images --onefile --version-file "../versionfile.txt" 
 POPD
