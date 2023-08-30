cf182628deb6a08ad2a0fac39a85d41c5423aa34
lexxai <lexxai@users.noreply.github.com>
2023-08-31 00:45:24 +0300

    build version tune

--- script/build-version.py
+++ script/build-version.py
@@ -1,23 +1,36 @@
 import pyinstaller_versionfile
 from configparser import ConfigParser
+import sys
+from pathlib import Path
 
 
 parser = ConfigParser()
 # read config file
-parser.read("..\setup.cfg")
+config_setup=Path("..\setup.cfg")
+if config_setup.is_file():
+    parser.read(config_setup)
+    ver = parser["metadata"].get("version", "0.0.1")
+else:
+    config_setup=Path("..\pyproject.toml")
+    if config_setup.is_file():
+        parser.read(config_setup)
+        ver = parser["tool.poetry"].get("version", "0.0.1")
+        ver = ver.strip('"')
 
-ver = parser["metadata"].get("version", "0.0.1")
-
-
-pyinstaller_versionfile.create_versionfile(
-    output_file="..\\versionfile.txt",
-    version=f"{ver}.0",
-    company_name="lexxai",
-    file_description="Using Python and OpenCV to detect the border of a white image on a gray background, crops and corrects its geometry.",
-    internal_name="ai_crop_images",
-    legal_copyright="https://github.com/lexxai/crop_and_skew_white_image_on_gray",
-    original_filename="ai_crop_images.exe",
-    product_name="ai_crop_images",
-)
-
-print(f"Done: versionfile.txt in parent folder. version='{ver}.0'")
+if len(sys.argv) > 1:
+    filename = Path(sys.argv[1])
+    if filename.is_file():
+        new_fn = filename.with_stem(f"{filename.stem}_{ver}")
+        filename.rename(new_fn)
+else:
+    pyinstaller_versionfile.create_versionfile(
+        output_file="..\\versionfile.txt",
+        version=f"{ver}.0",
+        company_name="Xuno",
+        file_description="Using Python and OpenCV to split images by segments and try analyze and OCR it.",
+        internal_name="ai_crop_images",
+        legal_copyright="https://github.com/xuno/",
+        original_filename=f"ai_crop_images_{ver}.exe",
+        product_name="ai_crop_images",
+    )
+    print(f"Done: versionfile.txt in parent folder. version='{ver}.0'")
--- script/build.cmd
+++ script/build.cmd
@@ -1,8 +1,7 @@
 python build-version.py
-
 mkdir "../pyinstall"
 rem ERASE "../pyinstall" /S/Q
 PUSHD "../pyinstall"
-
-pyinstaller "../ai_crop_images/main.py" --clean --name ai_crop_images --onefile --version-file "../versionfile.txt" 
+pyinstaller "../ai_crop_images/main.py" --clean --name ai_crop_images --onefile --version-file "../versionfile.txt"
 POPD
+python build-version.py ../pyinstall/dist/ai_crop_images.exe
